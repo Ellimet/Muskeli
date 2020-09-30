422ba62d865f16e7efa2bb1329bdc063799cf03f
Elias Perttu <eliasperttu@gmail.com>
2020-09-30 15:36:03 +0300

    Pohja sille tiedolle mitä harjoituksista tarvitaan

---
+++ app/src/main/java/com/example/myapplication/ExerciseData.java
@@ -0,0 +1,67 @@
+package com.example.myapplication;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExerciseData {
+
+    public static String[] names = {
+            "Knee push-up",
+            "Elbow raise",
+            "Scapular Push",
+            "Squat",
+            "Three way lunge",
+            "Plank opposite knee to elbow",
+            "Jumping jacks",
+            "ISO crunches",
+            "Recliner elbow to knee tucks",
+            "Glute bridge",
+            "Push-up",
+            "Spiderman pushup",
+            "Pike push-up",
+            "Beginner Shrimp",
+            "Floor bridge half rotation",
+            "Supermans",
+            "Starfish crunches",
+            "Windshield wipers",
+            "Heels to the heavens",
+            "Burpee",
+            "Split squat jumps",
+            "Squat jumps",
+            "Single leg RDL",
+            "Bodyweight side lateral raises",
+            "Diamond push-up",
+            "Plyo pushup",
+            "Archer push-up",
+            "Hindu push-up",
+            "Squat thrust x push-up",
+            "Bridge",
+            "Coussac squat",
+            "Burpee",
+            "Starfish crunches",
+            "Windshield wipers",
+            "Heels to the heavens"
+    };
+
+    public static Exercise[] easy = {
+            Exercise.KNEE_PUSH_UP,
+            Exercise.ELBOW_RAISE,
+            Exercise.SPACULAR_PUSH,
+
+    };
+    public static Exercise[] medium = {
+            Exercise.KNEE_PUSH_UP,
+            Exercise.ELBOW_RAISE,
+            Exercise.SPACULAR_PUSH,
+
+
+    };
+    public static Exercise[] hard = {
+            Exercise.KNEE_PUSH_UP,
+            Exercise.ELBOW_RAISE,
+            Exercise.SPACULAR_PUSH,
+
+    };
+
+
+}
