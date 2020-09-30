b5eefc2a3eb4b59c2f7600d7dbe23eeb106fa1ee
Ronttipossu <elmo.vahvaselka@hotmail.com>
2020-09-30 15:53:41 +0300

    Exercise.javan siistiminen

--- app/src/main/java/com/example/myapplication/Exercise.java
+++ app/src/main/java/com/example/myapplication/Exercise.java
@@ -3,37 +3,34 @@ package com.example.myapplication;
 public enum Exercise {
     KNEE_PUSH_UP,
     ELBOW_RAISE,
-    Scapular Push,
-    Squat
-    Three way lunge
-    Three way lunge
-    Plank opposite knee to elbow
-    Jumping jacks
-    ISO crunches
-    Recliner elbow to knee tucks
-    Glute bridge
-    Keskitaso
-    Spiderman pushup
-    Pike push-up
-    Beginner Shrimp
-    Floor bridge half rotation
-    Supermans
-    Starfish crunches
-    Windshield wipers
-    Heels to the heavens
-    Burpee
-    Split squat jumps
-    Squat jumps
-    Single leg RDL
-    Bodyweight side lateral raises
-    Diamond push-up
-    Plyo pushup
-    Archer push-up
-    Hindu push-up
-    Squat thrust x push-up Bridge
-    Coussac squat
-    Burpee
-    Starfish crunches
-    Windshield wipers
-    HEELS_TO_THE_HEAVENS
+    SCAPULAR_PUSH,
+    SQUAT,
+    THREE_WAY_LUNGE,
+    PLANK_OPPOSITE_KNEE_TO_ELBOEW,
+    JUMPING_JACKS,
+    ISO_CRUNCHES,
+    RECLINER_ELBOW_TO_KNEE_TUCKS,
+    GLUTE_BRIDGE,
+    //Keskitaso
+    SPIDERMNA_PUSHUP,
+    PIKE_PUSHUP,
+    BEGINNER_SHRIMP,
+    FLOOR_BRIDGE_HALF_ROTATION,
+    SUPERMANS,
+    STARFISH_CRUNCHES,
+    WINDSHIELD_WIPERS,
+    HEELS_TO_THE_HEAVENS,
+    BURPEE,
+    SPLIT_SQUAT_JUMPS,
+    SQUAT_JUMPS,
+    SINGLE_LEG_RDL,
+    //Haastava
+    BODYWEIGHT_SIDE_LATERAL_RAISES,
+    DIAMOND_PUSHUP,
+    PLYO_PUSHUP,
+    ARCHER_PUSHUP,
+    HINDU_PUSHUP,
+    SQUAT_THRUST_X_PUSHUP,
+    BRIDGE,
+    COUSSAC_SQUAT,
     }
