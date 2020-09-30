802ea219ba8be7071cf868d42470d96ca925b115
Ellimet <elias.perttu@gmail.com>
2020-09-30 11:43:56 +0300

    Elmon ylistys testi2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
- //Elmo osaa mutta sami eiads 
+ //Elmo osaa mutta sami eiads
+    //perkl
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
