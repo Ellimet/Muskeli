1450be435be8acf7a46465afeecfaa52b5c785eb
Ellimet <elias.perttu@gmail.com>
2020-09-30 15:07:12 +0300

    Lisättiin enum harjoitukset siivottavaksi

---
+++ app/src/main/java/com/example/myapplication/Harjoitus.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+public enum Harjoitus {
+    KNEE_PUSH_UP,
+    ELBOW_RAISE,
+    Scapular Push
+    Squat
+    Three way lunge
+    Three way lunge
+    Plank opposite knee to elbow
+    Jumping jacks
+    ISO crunches
+    Recliner elbow to knee tucks
+    Glute bridge
+    Keskitaso
+    Spiderman pushup
+    Pike push-up
+    Beginner Shrimp
+    Floor bridge half rotation
+    Supermans
+    Starfish crunches
+    Windshield wipers
+    Heels to the heavens
+    Burpee
+    Split squat jumps
+    Squat jumps
+    Single leg RDL
+    Bodyweight side lateral raises
+    Diamond push-up
+    Plyo pushup
+    Archer push-up
+    Hindu push-up
+    Squat thrust x push-up Bridge
+    Coussac squat
+    Burpee
+    Starfish crunches
+    Windshield wipers
+    HEELS_TO_THE_HEAVENS
+    }
