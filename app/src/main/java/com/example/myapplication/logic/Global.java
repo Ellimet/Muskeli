f0396755d732132962d3ad37ed853280a9532c0d
Ellimet <elias.perttu@gmail.com>
2020-10-01 16:26:42 +0300

    Elmon ylistys testi2

--- app/src/main/java/com/example/myapplication/logic/Global.java
+++ app/src/main/java/com/example/myapplication/logic/Global.java
@@ -2,25 +2,38 @@ package com.example.myapplication.logic;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Global {
-    private List<Exercise> exercises;
+
+    public Random rng;
+
+    private Exercise[] exercises;
+    private int difficulty;
+
     private static final Global singleton = new Global();
 
     private Global() {
-        exercises = new ArrayList<>();
+        rng = new Random(1337);
     }
 
     public static Global getInstance() {
         return singleton;
     }
 
-    public List<Exercise> getExercises() {
+    public Exercise[] getExercises() {
         return exercises;
     }
 
-    public List<Exercise> renewExercises(int difficulty) {
-        this.exercises = new ArrayList<>();
+    public Exercise[] setExercises(Exercise[] e) {
+        this.exercises = e;
+    }
+    public int getDifficulty() {
+        return this.difficulty;
+    }
+
+    public Exercise[] renewExercises() {
+        this.exercises = ExerciseData.easy;
         if (difficulty == 0) {
             //arvotaan
 
@@ -30,6 +43,15 @@ public class Global {
 
         }
         return exercises;
+    }
+    public Exercise[] renewExercise(int index) {
+        int harjoitus = rng.nextInt(6);
+        this.exercises[index] = getExercisesByDifficulty()[harjoitus];
+        return this.exercises;
+    }
+
+    private Exercise[] getExercisesByDifficulty() {
+        return this.difficulty == 0 ? ExerciseData.easy :  this.difficulty == 1 ? ExerciseData.medium : ExerciseData.hard;
     }
 }
 
